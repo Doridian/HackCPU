01caf8646daff865825953b7c6ec244b29a336f3
Mark Dietzer <mriq91@gmail.com>
2017-11-09 17:47:27 +0100

    unset all interrupts

--- asm/bootloader.asm
+++ asm/bootloader.asm
@@ -5,6 +5,8 @@ XOR $R1 $R1
 XOR $R2 $R2
 XOR $R3 $R3
 XOR $R4 $R4
+
+# Read ROM to RAM
 PUSH 1
 INT 6
 POP $R1
@@ -20,20 +22,31 @@ PUSH $R1
 PUSH $R2
 PUSH 1
 INT 1
+
 MOV $IHBASE $R2
 # We need 512 (256 * 2) space, but since we can use the ROM's first 4 bytes which are only needed to boot (enc key) and then zero'd by us
 SUB $IHBASE 508
+
 MOV $PSP $IHBASE
 SUB $PSP 0x100
 MOV $CSP $PSP
 SUB $CSP 0x100
+
 MOV32 $R34 @$R2
 MOV32 @$R2 0
 XOR $R3 0xDEFA
 XOR $R4 0xBEBA
+
 ADD $R2 4
 MOV @$CSP $R2
 ADD $CSP 2
+
+MOV $R1 256
+:unset_ih
+SUB $R1 1
+SETIH $R1 0
+JNZ $R1 :unset_ih
+
 ENCOFF
 :DISABLE_ENC
 MOV32 $ENCREG $R34

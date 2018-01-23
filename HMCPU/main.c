427e2f87aa2a8693625f1ed58f5ecde573de0243
Mark Dietzer <mriq91@gmail.com>
2018-01-23 18:36:05 +0100

    More information on exit

--- HMCPU/main.c
+++ HMCPU/main.c
@@ -8,6 +8,18 @@
 #include "io.h"
 #include "memory.h"
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
+
 static void stdout_write(struct iostream_t* io, uint8_t i) {
 	putc(i, stdout);
 }
@@ -89,7 +101,7 @@ int main(int argc, const char **argv) {
 		printf("\n");
 	}
 
-	printf("Registers:\nR1=%08x R2=%08x R3=%08x R4=%08x R5=%08x R6=%08x\nPSP=%08x CSP=%08x PC=%08x IHBASE=%08x ENCREG=%016" PRIx64 "\n", r.r1, r.r2, r.r3, r.r4, r.r5, r.r6, r.psp, r.csp, r.pc, r.ihbase, r.encreg12);
+	printf("Registers:\nR1=0x%08x R2=0x%08x R3=0x%08x R4=0x%08x R5=0x%08x R6=0x%08x\nPSP=0x%08x CSP=0x%08x PC=0x%08x IHBASE=0x%08x\nFLAG=0b" BYTE_TO_BINARY_PATTERN " ENCREG=0x%016" PRIx64 "\n", r.r1, r.r2, r.r3, r.r4, r.r5, r.r6, r.psp, r.csp, r.pc, r.ihbase, BYTE_TO_BINARY(r.flagr), r.encreg12);
 
 	printf("Memory around PC(-1) region:");
 	int j = 0;
